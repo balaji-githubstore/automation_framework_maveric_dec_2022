2f3591a196a952221d9d336b273409a8f2da24b3
balaji-githubstore <balaji0017@gmail.com>
2023-01-03 16:25:35 +0530

    Added Placeholder Test Method

--- EmployeeManagement/LoginUITest.cs
+++ EmployeeManagement/LoginUITest.cs
@@ -5,7 +5,6 @@ namespace EmployeeManagement
 {
     public class LoginUITest
     {
-
         [Test]
         public void ValidateTitleTest()
         {
@@ -18,5 +17,22 @@ namespace EmployeeManagement
             string actualTitle = driver.Title;
             Assert.That(actualTitle, Is.EqualTo("OrangeHRM"));
         }
+
+        [Test]
+        public void ValidatePlaceholderTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
+            driver.Url = "https://opensource-demo.orangehrmlive.com/";
+
+            string actualUsernamePlaceholder=driver.FindElement(By.Name("username")).GetAttribute("placeholder");
+            string actualPasswordPlaceholder = driver.FindElement(By.Name("password")).GetAttribute("placeholder");
+
+            Assert.That(actualUsernamePlaceholder, Is.EqualTo("Username"));
+            Assert.That(actualPasswordPlaceholder, Is.EqualTo("Password"));
+        }
+
     }
 }
