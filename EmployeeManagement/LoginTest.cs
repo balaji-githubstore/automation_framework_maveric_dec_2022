edeadb3660d76313ceb76a910042c114b95f3dab
balaji-githubstore <balaji0017@gmail.com>
2023-01-03 17:30:44 +0530

    Added Browser Configuration Under Base - Inheritance

---
+++ EmployeeManagement/LoginTest.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeManagement.Base;
+
+namespace EmployeeManagement
+{
+    public class LoginTest : AutomationWrapper
+    {
+
+        [Test]
+        public void ValidLoginTest()
+        {
+            driver.FindElement(By.Name("username")).SendKeys("Admin");
+            //enter password as admin123
+            //click on login 
+
+            //Assert the url 
+        }
+
+
+    }
+}
--- EmployeeManagement/LoginUITest.cs
+++ EmployeeManagement/LoginUITest.cs
@@ -1,28 +1,11 @@
+using EmployeeManagement.Base;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace EmployeeManagement
 {
-    public class LoginUITest
+    public class LoginUITest : AutomationWrapper
     {
-        IWebDriver driver;
-
-        [SetUp]
-        public void BeforeMethod()
-        {
-            driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
-
-            driver.Url = "https://opensource-demo.orangehrmlive.com/";
-        }
-
-        [TearDown]
-        public void AfterMethod()
-        {
-            driver.Quit();
-        }
-
         [Test]
         public void ValidateTitleTest()
         {
