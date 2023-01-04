1f1373ad81baeb6914c6a2dda7c04a4f08ea8bd7
balaji-githubstore <balaji0017@gmail.com>
2023-01-04 17:47:14 +0530

    Created Utilities For handling TestCaseSource data

--- EmployeeManagement/LoginTest.cs
+++ EmployeeManagement/LoginTest.cs
@@ -6,12 +6,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EmployeeManagement.Base;
+using EmployeeManagement.Utilities;
 
 namespace EmployeeManagement
 {
     public class LoginTest : AutomationWrapper
     {
-
         [Test]
         public void ValidLoginTest()
         {
@@ -23,31 +23,7 @@ namespace EmployeeManagement
             Assert.That(actualUrl, Is.EqualTo("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"));
         }
 
-
-        public static object[] InvalidLoginData()
-        {
-            string[] dataSet1 = new string[3];
-            dataSet1[0] = "john";
-            dataSet1[1] = "john123";
-            dataSet1[2] = "Invalid credential";
-
-            string[] dataSet2 = new string[3];
-            dataSet2[0] = "peter";
-            dataSet2[1] = "peter123";
-            dataSet2[2] = "Invalid credential";
-
-            object[] allDataSets=new object[2];
-
-            allDataSets[0] = dataSet1;
-            allDataSets[1] = dataSet2;
-
-            return allDataSets;
-        }
-
-        [Test,TestCaseSource(nameof(InvalidLoginData))]
-        //[TestCase("john", "john123", "Invalid credential")]
-        //[TestCase("peter", "peter233", "Invalid credential")]
-        //[TestCase("saul", "saul123", "Invalid credential")]
+        [Test,TestCaseSource(typeof(DataSource), nameof(DataSource.InvalidLoginData))]
         public void InvalidLoginTest(string username, string password, string expectedError)
         {
             driver.FindElement(By.Name("username")).SendKeys(username);
---
+++ EmployeeManagement/Utilities/DataSource.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Utilities
+{
+    public class DataSource
+    {
+        public static object[] InvalidLoginData()
+        {
+            string[] dataSet1 = new string[3];
+            dataSet1[0] = "john";
+            dataSet1[1] = "john123";
+            dataSet1[2] = "Invalid credential";
+
+            string[] dataSet2 = new string[3];
+            dataSet2[0] = "peter";
+            dataSet2[1] = "peter123";
+            dataSet2[2] = "Invalid credential";
+
+            object[] allDataSets = new object[2];
+
+            allDataSets[0] = dataSet1;
+            allDataSets[1] = dataSet2;
+
+            return allDataSets;
+        }
+
+
+    }
+}
