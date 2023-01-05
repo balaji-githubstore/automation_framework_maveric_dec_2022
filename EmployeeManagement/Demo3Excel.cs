64c09ade221df8f1a07528ef0bccb71ba131bdc6
balaji-githubstore <balaji0017@gmail.com>
2023-01-05 17:54:21 +0530

    Excel read and printed all cell value

--- EmployeeManagement/Demo3Excel.cs
+++ EmployeeManagement/Demo3Excel.cs
@@ -3,17 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClosedXML.Excel;
 
 namespace EmployeeManagement
 {
+    //will be deleted
     public class Demo3Excel
     {
 
         [Test]
         public void DemoExcelRead()
         {
+            XLWorkbook book = new XLWorkbook(@"C:\Mine\Company\Maveric C# 2022\AutomationFramework\EmployeeManagement\TestData\orange_data.xlsx");
 
-          
+            IXLWorksheet sheet = book.Worksheet("InvalidLoginTest");
+
+            IXLRange range = sheet.RangeUsed();
+
+
+            object[] allData = new object[3];
+
+            for (int r = 2; r <= 4; r++)
+            {
+                //create string[]
+                for (int c = 1; c <= 3; c++)
+                {
+                    string value = range.Cell(r, c).GetString();
+                    Console.WriteLine(value);
+                }
+
+                //load string[] to object[]
+               
+            }
+
+            book.Dispose();
+
+            //make sure object[] is ready with all test case
         }
     }
 }
