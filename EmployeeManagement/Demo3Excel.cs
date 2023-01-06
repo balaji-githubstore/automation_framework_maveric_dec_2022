f92f8a1590c93a7352b4bb8378916f21ab85b100
balaji-githubstore <balaji0017@gmail.com>
2023-01-06 15:25:29 +0530

    excel to object[] demo

--- EmployeeManagement/Demo3Excel.cs
+++ EmployeeManagement/Demo3Excel.cs
@@ -20,25 +20,59 @@ namespace EmployeeManagement
 
             IXLRange range = sheet.RangeUsed();
 
+            
+            object[] allData = new object[2];
 
-            object[] allData = new object[3];
-
-            for (int r = 2; r <= 4; r++)
+            for (int r = 2; r <= 3; r++)
             {
-                //create string[]
+                //array for one test case//one row
+                string[] arr = new string[3];
+
                 for (int c = 1; c <= 3; c++)
                 {
                     string value = range.Cell(r, c).GetString();
                     Console.WriteLine(value);
+                    arr[c-1] = value;
                 }
 
                 //load string[] to object[]
-               
+                allData[r-2] = arr;
             }
 
             book.Dispose();
 
             //make sure object[] is ready with all test case
+        }
+
+        [Test]
+        public void Demo2ExcelWrite()
+        {
+            using (XLWorkbook book = new XLWorkbook(@"C:\Mine\Company\Maveric C# 2022\AutomationFramework\EmployeeManagement\TestData\orange_data.xlsx"))
+            {
+                IXLWorksheet sheet = book.Worksheet("InvalidLoginTest");
+
+                IXLRange range = sheet.RangeUsed();
+
+                int rowCount = range.RowCount();
+                int columnCount = range.ColumnCount();
+
+                object[] allData = new object[rowCount - 1];
+
+                for (int r = 2; r <= rowCount; r++)
+                {
+                    string[] arr = new string[3];
+
+                    for (int c = 1; c <= columnCount; c++)
+                    {
+                        string value = range.Cell(r, c).GetString();
+                        Console.WriteLine(value);
+                        arr[c - 1] = value;
+                    }
+                    allData[r - 2] = arr;
+                }
+                
+            }
+
         }
     }
 }
