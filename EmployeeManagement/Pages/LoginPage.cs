d85ff2c379d0cd37988501308f455e5769b19268
balaji-githubstore <balaji0017@gmail.com>
2023-01-09 15:44:55 +0530

    Code without page object model

--- EmployeeManagement/Pages/LoginPage.cs
+++ EmployeeManagement/Pages/LoginPage.cs
@@ -1,24 +1,24 @@
-﻿using DocumentFormat.OpenXml.Bibliography;
-using OpenQA.Selenium;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using DocumentFormat.OpenXml.Bibliography;
+//using OpenQA.Selenium;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace EmployeeManagement.Pages
-{
-    public class LoginPage
-    {
-        public static void EnterUsername(IWebDriver driver,string username)
-        {
-            driver.FindElement(By.Name("username")).SendKeys(username);
-        }
+//namespace EmployeeManagement.Pages
+//{
+//    public class LoginPage
+//    {
+//        public static void EnterUsername(IWebDriver driver,string username)
+//        {
+//            driver.FindElement(By.Name("username")).SendKeys(username);
+//        }
 
-        public static void EnterPassword(IWebDriver driver,string password)
-        {
-            driver.FindElement(By.Name("password")).SendKeys(password);
-        }
+//        public static void EnterPassword(IWebDriver driver,string password)
+//        {
+//            driver.FindElement(By.Name("password")).SendKeys(password);
+//        }
 
-    }
-}
+//    }
+//}
